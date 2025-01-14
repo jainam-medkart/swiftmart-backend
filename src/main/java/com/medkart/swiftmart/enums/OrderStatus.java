104ec945d4136e36ed38650eefe3e40d32213110
Jainam Panchal <jainampanchal1@gmail.com>
2025-01-14 12:16:28 +0530

    Updated schema to accommodate payments

---
+++ src/main/java/com/medkart/swiftmart/enums/OrderStatus.java
@@ -0,0 +1,5 @@
+package com.medkart.swiftmart.enums;
+
+public enum OrderStatus {
+    PENDING, CONFIRMED , DELIVERING , DELIVERED , CANCELLED , REFUNDED;
+}
---
+++ src/main/java/com/medkart/swiftmart/enums/UserRole.java
@@ -0,0 +1,5 @@
+package com.medkart.swiftmart.enums;
+
+public enum UserRole {
+    ADMIN, USER
+}
