fb60e2fd5511f4e3f20c8b8508375dae75c84aa8
Jainam Panchal <jainampanchal1@gmail.com>
2025-01-14 10:05:07 +0530

    Updated Dependencies

---
+++ src/main/java/com/medkart/swiftmart/SwiftmartApplication.java
@@ -0,0 +1,13 @@
+package com.medkart.swiftmart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwiftmartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwiftmartApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/medkart/swiftmart/SwiftmartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medkart.swiftmart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwiftmartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
